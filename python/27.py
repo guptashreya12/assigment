e55abe9b8f5d2e9cfcf6c794233bf94419b2d8c0
guptashreya12 <shreyajune19@gmail.com>
2024-06-18 19:09:52 +0530

    assignment

---
+++ python/27.py
@@ -0,0 +1,7 @@
+
+# Program to convert a string to a list of characters
+def string_to_list(string):
+    return list(string)
+
+user_input = input("Enter a string: ")
+print(f"The list of characters is: {string_to_list(user_input)}")
